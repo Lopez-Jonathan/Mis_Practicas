a29865fdecc347847b7f6fdef68765d674d9b788
Lopez-Jonathan <89488456+Lopez-Jonathan@users.noreply.github.com>
2021-11-01 16:52:58 -0300

    Add files via upload

--- Clase 9/main.js
+++ Clase 9/main.js
@@ -1,5 +1,7 @@
+alert("¿Esta dispuesto a ver el mejor sitio de recetas?");
 const btnSwitch=document.querySelector("#switch");
-const btnAbrir=document.querySelector("#abrir-menu")
+const btnAbrir=document.querySelector("#abrir-menu");
+const btnInformacion=document.querySelector("#boton-footer")
 
 btnSwitch.addEventListener('click' , () =>{
     document.body.classList.toggle('dark');
@@ -9,3 +11,9 @@ btnSwitch.addEventListener('click' , () =>{
 btnAbrir.addEventListener('click' , () =>{
   document.querySelector('.nav-links-center').classList.toggle('mostrar');
 });
+
+btnInformacion.addEventListener('click', ()=>{
+  swal.fire({
+    html: '<h1>Informacion Del Autor</h1> <p>Nombre:Jonathan Lopez</p> <p>Mail: Jonathan_Nahuel_Lop@hotmail.com</p> <p>Facebook<i class="fab fa-facebook"></i>:https://www.facebook.com/jony.lopez.3597/</p> <p>Instagram<i class="fab fa-instagram"></i>:https://www.instagram.com/jona_nahu/</p> <p>Whatsapp<i class="fab fa-whatsapp"></i>:+54 1165105251</p>'
+  })
+})
