22a8fffc0629dd021b214d66c3475048b196186f
Lopez-Jonathan <89488456+Lopez-Jonathan@users.noreply.github.com>
2021-11-29 22:47:03 -0300

    Add files via upload

--- Proyecto Final/main.js
+++ Proyecto Final/main.js
@@ -1,1 +1,6 @@
-
+const btnAbrir=document.querySelector("#switch");
+btnAbrir.addEventListener('click' , () =>{
+  document.querySelector('#menu-desplegable').classList.toggle('menu-desplegable-abierto');
+  document.body.classList.toggle('blur');
+  document.querySelector('.abrir-menu').classList.toggle('menu-cruz');
+});
