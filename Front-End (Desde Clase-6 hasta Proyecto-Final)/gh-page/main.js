cf967f127c3a513001d201cef1ef894405887fac
Lopez-Jonathan <89488456+Lopez-Jonathan@users.noreply.github.com>
2025-04-15 21:09:11 -0300

    Delete Front-End (Desde Clase-6 hasta Proyecto-Final)/gh-page directory

--- Front-End (Desde Clase-6 hasta Proyecto-Final)/gh-page/main.js
+++
@@ -1,6 +0,0 @@
-const btnAbrir=document.querySelector("#switch");
-btnAbrir.addEventListener('click' , () =>{
-  document.querySelector('#menu-desplegable').classList.toggle('menu-desplegable-abierto');
-  document.body.classList.toggle('blur');
-  document.querySelector('.abrir-menu').classList.toggle('menu-cruz');
-});
